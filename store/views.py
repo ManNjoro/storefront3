1eaa83c54fc76ed95d6d9e89525bfa22f57e541e
ManNjoro <elijohnmwho@gmail.com>
2025-03-09 19:07:17 +0300

    Added ProductImageSerializer and updated ProductSerializer and ProductViewSet to include product images.

--- store/views.py
+++ store/views.py
@@ -20,7 +20,7 @@ from .serializers import (AddCartItemSerializer, CartItemSerializer, CartSeriali
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.prefetch_related('images').all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
