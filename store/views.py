21544109802ce8014ab79d23938ffda4e0871441
ManNjoro <elijohnmwho@gmail.com>
2025-03-09 19:00:42 +0300

    Added product image management to store API

--- store/views.py
+++ store/views.py
@@ -149,5 +149,8 @@ class OrderViewSet(ModelViewSet):
 class ProductImageViewSet(ModelViewSet):
     serializer_class = ProductImageSerializer
     
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
+    
     def get_queryset(self):
         return ProductImage.objects.filter(product_id=self.kwargs['product_pk'])
