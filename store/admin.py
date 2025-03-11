c97a2be1eedb8b5204d06b39d6bf7570d2238797
ManNjoro <elijohnmwho@gmail.com>
2025-03-11 21:40:46 +0300

    Added ProductImageInline to CustomProductAdmin and ProductAdmin in admin.py files.

--- store/admin.py
+++ store/admin.py
@@ -19,6 +19,8 @@ class InventoryFilter(admin.SimpleListFilter):
         if self.value() == '<10':
             return queryset.filter(inventory__lt=10)
 
+class ProductImageInline(admin.TabularInline):
+    model = models.ProductImage
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -27,6 +29,7 @@ class ProductAdmin(admin.ModelAdmin):
         'slug': ['title']
     }
     actions = ['clear_inventory']
+    inlines = [ProductImageInline]
     list_display = ['title', 'unit_price',
                     'inventory_status', 'collection_title']
     list_editable = ['unit_price']
