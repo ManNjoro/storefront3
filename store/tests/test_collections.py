1b8de23429b07beb50bd2091da9a1d97a71a0096
ManNjoro <elijohnmwho@gmail.com>
2025-03-12 14:17:47 +0300

    Removed Arrange section and added response assignment directly in test method.

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,13 +1,10 @@
 from rest_framework import status
 from rest_framework.test import APIClient
 
+
 class TestCreateCollection:
     def test_if_user_is_anonymous_returns_401(self):
-        # Arrange
-        
-        # Act
         client = APIClient()
-        response = client.post('/store/collections/', {'title': 'a'})
-        
-        # Assert
+        response = client.post("/store/collections/", {"title": "a"})
+
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
