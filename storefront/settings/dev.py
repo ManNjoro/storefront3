b611652a3033c1684a01b5f655dd87e8f01830a6
ManNjoro <elijohnmwoho@gmail.com>
2025-05-24 23:58:43 +0300

    `Update settings files to use Redis for Celery and caching`
    `storefront/settings/common.py`: Removed CELERY_BROKER_URL and CACHES settings.
    
    `storefront/settings/dev.py`: Added CELERY_BROKER_URL and CACH

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -13,3 +13,16 @@ DATABASES = {
         "PASSWORD": env("DB_PASSWORD"),
     }
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "TIMEOUT": 10 * 60,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
