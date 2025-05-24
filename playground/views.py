c0b7ae95ba50a632c88367c052ba39d0b59f6a1d
ManNjoro <elijohnmwoho@gmail.com>
2025-05-24 17:00:58 +0300

    Added caching for HTTP request result in playground/views.py and set cache timeout in storefront/settings.py

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,13 @@
+from django.core.cache import cache
 from django.shortcuts import render
 # from .tasks import notify_customers
 import requests
 
 def say_hello(request):
     # notify_customers.delay('Hello')
-    requests.get('https://httpbin.org/delay/2')
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    key = 'httpbin_result'
+    if cache.get(key) is None:
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        cache.set(key, data)
+    return render(request, 'hello.html', {'name': cache.get(key)})
