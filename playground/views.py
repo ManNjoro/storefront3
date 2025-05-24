495b20ed71ae048ca569421270eb08213e8af209
ManNjoro <elijohnmwoho@gmail.com>
2025-05-24 17:40:19 +0300

    Added cache_page decorator to HelloView's get method and imported method_decorator

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,13 @@
 from django.core.cache import cache
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 # from .tasks import notify_customers
 from rest_framework.views import APIView
 import requests
 
 class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
     def get(self, request):
         response = requests.get('https://httpbin.org/delay/2')
         data = response.json()
