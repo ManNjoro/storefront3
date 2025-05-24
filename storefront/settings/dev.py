6f3632e4d722c9e32b4dc7a14526efb5c55f15af
ManNjoro <elijohnmwoho@gmail.com>
2025-05-25 01:49:47 +0300

    `Update celery and settings for development environment`
    or
    
    `Update storefront celery and settings for dev environment`

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -9,17 +9,17 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         "NAME": env("DB_NAME"),
         "USER": env("DB_USER"),
-        "HOST": env("DB_HOST"),
+        "HOST": 'mysql',
         "PASSWORD": env("DB_PASSWORD"),
     }
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://redis:6379/1'
 
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
+        "LOCATION": "redis://redis:6379/2",
         "TIMEOUT": 10 * 60,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
@@ -27,7 +27,11 @@ CACHES = {
     }
 }
 
-EMAIL_HOST ='localhost'
+EMAIL_HOST ='smtp4dev'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True
+}
